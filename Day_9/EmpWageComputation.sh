56b3b490a72b54a5b022fdbb9872ac731aada6aa
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-04 19:49:42 +0530

    We Can Find How Much Wage Employee Earn Today

--- Day_9/EmpWageComputation.sh
+++ Day_9/EmpWageComputation.sh
@@ -6,9 +6,17 @@ echo "Welcome to Employee Wage Computation Prgram on Master Branch"
 
 	EmpAttendance=$((RANDOM%2))
 
+	EmpWagePerHr=20
+
 	if [ $EmpAttendance -eq 0 ]
 	then
 		echo "Employee is Absent"
+		WorkingHrs=0
 	else
 		echo "Employee is Present"
+		WorkingHrs=8
 	fi
+
+	DailyWage=$(($WorkingHrs*$EmpWagePerHr))
+
+	echo "Employee got $DailyWage Today"
