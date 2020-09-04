a996d02623128f400082cf28fd7189315fc3e420
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-04 16:43:18 +0530

    Employee is Present or Absent

--- Day_9/EmpWageComputation.sh
+++ Day_9/EmpWageComputation.sh
@@ -2,3 +2,13 @@
 
 echo "Welcome to Employee Wage Computation Prgram on Master Branch"
 
+
+
+	EmpAttendance=$((RANDOM%2))
+
+	if [ $EmpAttendance -eq 0 ]
+	then
+		echo "Employee is Absent"
+	else
+		echo "Employee is Present"
+	fi
