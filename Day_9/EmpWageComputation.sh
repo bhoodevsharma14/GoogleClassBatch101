605bf3400d9aa0bd6b4b9cc3e2f4a0ab6432ac9d
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-05 16:28:26 +0530

    Storing and Printing Daily Wage with there Respective Days

--- Day_9/EmpWageComputation.sh
+++ Day_9/EmpWageComputation.sh
@@ -6,10 +6,19 @@
 	TotalWorkingHrs=100
 	EmpWorkingHrs=0
 	WorkingDays=0
+	declare -A DailyWage_with_Day
 
 	function getWorkingHrs()
 	{
 		echo "Employee Work For $EmpWorkingHrs hr In This Month"
+	}
+
+	function  getDailyWageWithDate()
+	{
+		for day in ${!DailyWage_with_Day[@]}
+		do
+			echo "Day $day : ${DailyWage_with_Day[$day]}"
+		done
 	}
 
 
@@ -41,6 +50,8 @@
 
 		DailyWageData+=($DailyWage)
 
+		DailyWage_with_Day[$day]=$DailyWage
+
 		((TotalWage+=DailyWage))
 
 		if [ $EmpWorkingHrs -ge $TotalWorkingHrs ]
@@ -55,3 +66,5 @@
 	echo "Employee earn Rs.$TotalWage ,this Month"
 
 	echo "Employee Daily Wage Data is ${DailyWageData[@]}"
+
+	getDailyWageWithDate
